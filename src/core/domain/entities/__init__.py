53019d0347afce93d88e26b363553392589a6227
Lucas Cristovam <lucas.henriques@totvs.com.br>
2024-05-11 10:25:49 -0300

    docs: add base packages docstrings

---
+++ src/core/domain/entities/__init__.py
@@ -0,0 +1,6 @@
+"""Contains the main entities of the domain model.
+
+Entities are unique objects identified not by their attributes, but by their continuity and
+distinct identity.
+They form the core of the domain model, encapsulating both behavior and data.
+"""
---
+++ src/core/domain/value_objects/__init__.py
@@ -0,0 +1,10 @@
+"""This module defines the Value Objects used within the domain.
+
+Value Objects are immutable entities characterized by their attributes or state, rather than
+their identity.
+They encapsulate a set of attributes and related behavior
+that form a conceptual whole within the domain model.
+
+This module provides a declaration of such objects,
+promoting code consistency and domain integrity.
+"""
