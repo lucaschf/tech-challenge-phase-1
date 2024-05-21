742e390a45b285053bc2fd8b8dcac7e6c90acd8c
Lucas Cristovam <lucas.henriques@totvs.com.br>
2024-05-21 19:25:36 -0300

    feat: add customer use cases

---
+++ src/adapter/driven/infra/repositories/__init__.py
@@ -0,0 +1,3 @@
+from .sqa_customer_repository import SQACustomerRepository
+
+__all__ = ["SQACustomerRepository"]
---
+++ src/core/application/use_cases/customer_user_case_impl.py
@@ -0,0 +1,49 @@
+from src.core.application.use_cases.customer_use_case import CustomerUseCase
+from src.core.domain.base import DomainError
+from src.core.domain.entities import Customer
+from src.core.domain.repositories import CustomerRepository
+from src.core.domain.value_objects import CPF, Email
+
+
+class CustomerUserCaseImpl(CustomerUseCase):
+    """Implementation of the customer use case.
+
+    Attributes:
+        customer_repository (CustomerRepository): The repository for customer operations.
+    """
+
+    def __init__(self, customer_repository: CustomerRepository) -> None:
+        self.customer_repository = customer_repository
+
+    def create_customer(self, name: str, cpf: CPF, email: Email) -> Customer:
+        """Create a new customer.
+
+        Args:
+            name: The customer's name.
+            cpf: The customer's CPF.
+            email: The customer's email.
+
+        Returns:
+            Customer: The created customer data.
+
+        Raises:
+            DomainError: If the customer already exists.
+        """
+        customer: Customer = Customer(name=name, cpf=cpf, email=email)
+        if self.customer_repository.exists(customer.cpf, customer.email):
+            raise DomainError(message="Customer already exists")
+        return self.customer_repository.add(customer)
+
+    def get_by_cpf(self, cpf: str) -> Customer:
+        """Get a customer by their CPF.
+
+        Args:
+            cpf: The customer's CPF.
+
+        Returns:
+            Customer: The customer data if found.
+        """
+        return self.customer_repository.get_by_cpf(CPF(cpf))
+
+
+__all__ = ["CustomerUserCaseImpl"]
