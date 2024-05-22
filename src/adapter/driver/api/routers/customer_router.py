2d49a386d992ad537758ff4ee2fb54ba8c90fd76
Lucas Cristovam <lucas.henriques@totvs.com.br>
2024-05-22 20:52:48 -0300

    fix: rollback route get_by cpf removal

--- src/adapter/driver/api/routers/customer_router.py
+++ src/adapter/driver/api/routers/customer_router.py
@@ -6,6 +6,7 @@ from ..controllers import CustomerController
 from ..dependencies import injector
 from ..schemas import CustomerCreationIn, CustomerOut
 from ..schemas.http_error import HttpErrorOut
+from ..types import CPFStr
 
 router = APIRouter(tags=["Customer"], prefix="/customer")
 
@@ -27,6 +28,21 @@ def create_customer(
     customer = controller.create_customer(inputs)
     response.headers["Location"] = f"{router.prefix}/{customer.cpf}"
     return customer
+
+
+@router.get(
+    "/{cpf}",
+    responses={404: {"model": HttpErrorOut}, 400: {"model": HttpErrorOut}},
+    description="Retrieves a customer from the system using their CPF. The CPF is passed as a path "
+    "parameter.",
+)
+def get_by_cpf(
+    cpf: CPFStr,
+    controller: CustomerController = Depends(  # noqa: B008
+        lambda: injector.get(CustomerController)
+    ),
+) -> CustomerOut:
+    return controller.get_by_cpf(cpf)
 
 
 __all__ = ["router"]
