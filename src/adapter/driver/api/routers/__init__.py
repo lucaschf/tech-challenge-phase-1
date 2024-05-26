633b8c485267e1b42b2b5ec48d03141b6a96544e
Lucas Cristovam <lucas.henriques@totvs.com.br>
2024-05-26 19:03:18 -0300

    feat: include order router on api

--- src/adapter/driver/api/routers/__init__.py
+++ src/adapter/driver/api/routers/__init__.py
@@ -1,4 +1,5 @@
 from .customer_router import router as customer_router
+from .order_router import router as order_router
 from .product_router import router as product_router
 
-__all__ = ["customer_router", "product_router"]
+__all__ = ["customer_router", "order_router", "product_router"]
