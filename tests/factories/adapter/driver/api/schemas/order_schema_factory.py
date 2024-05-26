8e1a2ef061f8ab231a11618b70ea55e08372f51f
Lucas Cristovam <lucas.henriques@totvs.com.br>
2024-05-26 19:09:23 -0300

    fix: order schema factory

--- tests/factories/adapter/driver/api/schemas/order_schema_factory.py
+++ tests/factories/adapter/driver/api/schemas/order_schema_factory.py
@@ -2,7 +2,15 @@ from uuid import uuid4
 
 import factory
 
-from src.adapter.driver.api.schemas.order_schema import OrderIn
+from src.adapter.driver.api.schemas.order_schema import OrderIn, OrderProductIn
+
+
+class OrderProductInFactory(factory.Factory):  # noqa: D101
+    product_id = factory.LazyFunction(uuid4)
+    quantity = factory.Faker("random_int", min=1, max=5)
+
+    class Meta:  # noqa: D106
+        model = OrderProductIn
 
 
 class OrderInFactory(factory.Factory):  # noqa: D101
@@ -10,4 +18,4 @@ class OrderInFactory(factory.Factory):  # noqa: D101
         model = OrderIn
 
     user_id = factory.LazyFunction(uuid4)
-    products = factory.List(["product1", "product2"])
+    products = OrderProductInFactory.create_batch(2)
