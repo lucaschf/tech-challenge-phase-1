e612358fdc832d90191e4e64439c937de1dd1876
Lucas Cristovam <lucas.henriques@totvs.com.br>
2024-07-15 11:00:18 -0300

    test: fix test cases

--- tests/functional/test_order.py
+++ tests/functional/test_order.py
@@ -1,37 +1,47 @@
+from unittest.mock import ANY
+
 from fastapi.testclient import TestClient
 
 from src.adapter.driver.api.schemas.order_schema import OrderIn
 from tests.factories.adapter.driver.api.schemas.order_schema_factory import (
     OrderInFactory,
-    OrderProductInFactory,
+    OrderItemInFactory,
 )
 
 
-def test_checkout(client: TestClient, create_customer_in_db, create_products_in_db) -> None:  # noqa: ANN001
+def test_checkout(
+    client: TestClient,
+    create_customer_in_db,  # noqa: ANN001
+    create_products_in_db,  # noqa: ANN001
+) -> None:
     customer = create_customer_in_db
     products = create_products_in_db
 
-    order_products = [OrderProductInFactory(product_uuid=product.uuid) for product in products]
-    order_data: OrderIn = OrderInFactory(user_uuid=customer.uuid, products=order_products)
+    order_products = [OrderItemInFactory(product_id=product.uuid) for product in products]
+    order_data: OrderIn = OrderInFactory(customer_id=customer.uuid, items=order_products)
+
     response = client.post("/api/orders/checkout", json=order_data.model_dump(mode="json"))
-    assert response.status_code == 200
-    assert response.json()["user_uuid"] == str(order_data.user_uuid)
-    assert response.json()["status"] == "pending"
+
+    assert response.status_code == 201
+    assert response.json() == {"number": ANY}
 
 
-def test_list_orders(client: TestClient, create_customer_in_db, create_products_in_db) -> None:  # noqa: ANN001
+def test_list_orders(
+    client: TestClient,
+    create_customer_in_db,  # noqa: ANN001
+    create_products_in_db,  # noqa: ANN001
+) -> None:
     customer = create_customer_in_db
     products = create_products_in_db
 
-    order_products = [OrderProductInFactory(product_uuid=product.uuid) for product in products]
-    order_data: OrderIn = OrderInFactory(user_uuid=customer.uuid, products=order_products)
+    order_products = [OrderItemInFactory(product_id=product.uuid) for product in products]
+    order_data: OrderIn = OrderInFactory(customer_id=customer.uuid, items=order_products)
 
     client.post("/api/orders/checkout", json=order_data.model_dump(mode="json"))
 
     response = client.get("/api/orders")
     assert response.status_code == 200
     assert len(response.json()) > 0
-    assert response.json()[0]["user_uuid"] == str(order_data.user_uuid)
 
 
 def test_update_order_status(
@@ -42,11 +52,11 @@ def test_update_order_status(
     customer = create_customer_in_db
     products = create_products_in_db
 
-    order_products = [OrderProductInFactory(product_uuid=product.uuid) for product in products]
-    order_data: OrderIn = OrderInFactory(user_uuid=customer.uuid, products=order_products)
+    order_products = [OrderItemInFactory(product_id=product.uuid) for product in products]
+    order_data: OrderIn = OrderInFactory(customer_id=customer.uuid, items=order_products)
 
     create_response = client.post("/api/orders/checkout", json=order_data.model_dump(mode="json"))
-    order_uuid = create_response.json()["uuid"]
+    order_uuid = create_response.json()["number"]
 
     update_response = client.put(f"/api/orders/{order_uuid}/status", json={"status": "completed"})
 
